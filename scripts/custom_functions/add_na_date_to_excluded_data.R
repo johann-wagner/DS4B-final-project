e758931cf74c425c056159f8d7aea46bb04b80db
Johann Wagner <johann.wagner@gmail.com>
2023-10-11 22:13:09 +1100

    Modularise add_na_date_to_excluded_data

---
+++ scripts/custom_functions/add_na_date_to_excluded_data.R
@@ -0,0 +1,33 @@
+### ############################################################################
+### ############################################################################
+###                                                                          ###
+### DS4B-FINAL-PROJECT: add_na_date_to_excluded_data                         ###
+###                                                                          ###
+### ############################################################################
+### ############################################################################
+
+
+
+
+
+# add_na_date_to_excluded_data ----------------------------------------------
+
+### Purpose:
+# To add NA values from the eventDate column into the excluded data dataset.
+# Note: This does not remove/filter any data from the input data, this only 
+#       adds data to the excluded_data dataset.
+
+add_na_date_to_excluded_data <- function(species_occurrence) {
+  
+  add_excluded <- species_occurrence %>% 
+    filter(is.na(eventDate)) %>% 
+    
+    # Add excluded_notes
+    mutate(
+      date           = Sys.Date(),
+      excluded_notes = "These data points have a missing eventDate value."
+    )
+  
+  excluded_data <- bind_rows(excluded_data, add_excluded)
+  return(excluded_data)
+}
