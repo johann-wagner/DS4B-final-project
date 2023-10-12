43683e6e040e4cb7b96486439302774cb9b79967
Johann Wagner <johann.wagner@gmail.com>
2023-10-12 20:02:40 +1100

    Add data wrangling

--- scripts/3-01_data_wrangling.R
+++ scripts/3-01_data_wrangling.R
@@ -13,7 +13,7 @@
 # Setup and Configuration -------------------------------------------------
 
 source(
-  here(
+  here::here(
     "scripts",
     "0-00_setup_and_configuration.R"
   ),
@@ -25,6 +25,55 @@ source(
 
 
 
-# Data Wrangling ------------------------------------------------------------
+# Data Loading ------------------------------------------------------------
+
+source(
+  here::here(
+    "scripts",
+    "1-01_data_loading.R"
+  ),
+  echo = TRUE,
+  max.deparse.length = 1e4
+)
 
 
+
+
+
+# Data Cleaning ------------------------------------------------------------
+
+source(
+  here::here(
+    "scripts",
+    "2-01_data_cleaning.R"
+  ),
+  echo = TRUE,
+  max.deparse.length = 1e4
+)
+
+
+
+
+
+# Cleaned Data ------------------------------------------------------------
+
+# Remove the excluded_data from tidied_data
+cleaned_data <- tidied_data %>% 
+  anti_join(
+    excluded_data,
+    join_by(recordID)
+  )
+
+
+
+
+
+# Save Cleaned Data -----------------------------------------------------------
+
+cleaned_data %>% 
+  write_csv(
+    here(
+      "processed_data",
+      "cleaned_data.csv"
+    )
+  )
