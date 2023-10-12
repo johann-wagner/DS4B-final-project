cfe7c757c594051bb744530d97bbbbd07729e34a
Johann Wagner <johann.wagner@gmail.com>
2023-10-12 20:02:32 +1100

    Add data cleaning

--- scripts/2-01_data_cleaning.R
+++ scripts/2-01_data_cleaning.R
@@ -13,7 +13,7 @@
 # Setup and Configuration -------------------------------------------------
 
 source(
-  here(
+  here::here(
     "scripts",
     "0-00_setup_and_configuration.R"
   ),
@@ -25,6 +25,134 @@ source(
 
 
 
-# Data Cleaning ------------------------------------------------------------
+# Data Loading ------------------------------------------------------------
+
+source(
+  here::here(
+    "scripts",
+    "1-01_data_loading.R"
+  ),
+  echo = TRUE,
+  max.deparse.length = 1e4
+)
 
 
+
+
+
+# Data Cleaning - Excluded Data --------------------------------------------
+
+# Initialise/Create empty excluded_data tibble
+excluded_data <- tibble()
+
+# Excluded Data
+excluded_data <- raw_rabbits %>% 
+  add_na_date_to_excluded_data()
+
+excluded_data <- raw_rabbits %>% 
+  add_na_spatial_to_excluded_data()
+
+excluded_data <- raw_rabbits %>% 
+  filter(if_all(
+    c(eventDate, decimalLatitude, decimalLongitude), ~ !is.na(.))
+  ) %>% 
+  create_state_column() %>% 
+  add_not_within_abs_map_to_excluded_data()
+
+# Excluded Data
+excluded_data <- raw_foxes %>% 
+  add_na_date_to_excluded_data()
+
+excluded_data <- raw_foxes %>% 
+  add_na_spatial_to_excluded_data()
+
+excluded_data <- raw_foxes %>% 
+  filter(if_all(
+    c(eventDate, decimalLatitude, decimalLongitude), ~ !is.na(.))
+  ) %>% 
+  create_state_column() %>% 
+  add_not_within_abs_map_to_excluded_data()
+
+# Excluded Data
+excluded_data <- raw_cane_toads %>% 
+  add_na_date_to_excluded_data()
+
+excluded_data <- raw_cane_toads %>% 
+  add_na_spatial_to_excluded_data()
+
+excluded_data <- raw_cane_toads %>% 
+  filter(if_all(
+    c(eventDate, decimalLatitude, decimalLongitude), ~ !is.na(.))
+  ) %>% 
+  create_state_column() %>% 
+  add_not_within_abs_map_to_excluded_data()
+
+# Excluded Data
+excluded_data <- raw_cats %>% 
+  add_na_date_to_excluded_data()
+
+excluded_data <- raw_cats %>% 
+  add_na_spatial_to_excluded_data()
+
+excluded_data <- raw_cats %>% 
+  filter(if_all(
+    c(eventDate, decimalLatitude, decimalLongitude), ~ !is.na(.))
+  ) %>% 
+  create_state_column() %>% 
+  add_not_within_abs_map_to_excluded_data()
+
+# Excluded Data
+excluded_data <- raw_horses %>% 
+  add_na_date_to_excluded_data()
+
+excluded_data <- raw_horses %>% 
+  add_na_spatial_to_excluded_data()
+
+excluded_data <- raw_horses %>% 
+  filter(if_all(
+    c(eventDate, decimalLatitude, decimalLongitude), ~ !is.na(.))
+  ) %>% 
+  create_state_column() %>% 
+  add_not_within_abs_map_to_excluded_data()
+
+# Excluded Data
+excluded_data <- raw_pigs %>% 
+  add_na_date_to_excluded_data()
+
+excluded_data <- raw_pigs %>% 
+  add_na_spatial_to_excluded_data()
+
+excluded_data <- raw_pigs %>% 
+  filter(if_all(
+    c(eventDate, decimalLatitude, decimalLongitude), ~ !is.na(.))
+  ) %>% 
+  create_state_column() %>% 
+  add_not_within_abs_map_to_excluded_data()
+
+# Excluded Data
+excluded_data <- raw_red_fire_ants %>% 
+  add_na_date_to_excluded_data()
+
+excluded_data <- raw_red_fire_ants %>% 
+  add_na_spatial_to_excluded_data()
+
+excluded_data <- raw_red_fire_ants %>% 
+  filter(if_all(
+    c(eventDate, decimalLatitude, decimalLongitude), ~ !is.na(.))
+  ) %>% 
+  create_state_column() %>% 
+  add_not_within_abs_map_to_excluded_data()
+
+
+
+
+
+# Save Excluded Data -----------------------------------------------------------
+
+excluded_data %>% 
+  write_csv(
+    here(
+      "processed_data",
+      "excluded_data.csv"
+      )
+  )
