b76cc01b715b3137139a9fa9dd75e281a7085fd2
Johann Wagner <johann.wagner@gmail.com>
2023-10-12 22:58:49 +1100

    Create shinyapp

---
+++ invasive_species_dashboard/ui.R
@@ -0,0 +1,49 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Setup and Configuration -------------------------------------------------
+
+source(
+    here::here(
+        "scripts",
+        "0-00_setup_and_configuration.R"
+    ),
+    echo = TRUE,
+    max.deparse.length = 1e4
+)
+
+library(shiny)
+
+
+
+# Define UI for application that draws a histogram
+fluidPage(
+
+    # Application title
+    titlePanel("Invasive Species Dashboard"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("simpleName",
+                        label = "Species Name",
+                        choices = unique(dashboard_data$simpleName)
+                        ),
+            selectInput("state",
+                        label = "State Name",
+                        choices = unique(dashboard_data$state)
+            )
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("spatial_visualisation")
+        )
+    )
+)
