541abff26df268272931dace92823af81188a832
Johann Wagner <johann.wagner@gmail.com>
2023-10-14 17:16:09 +1100

    Add text

--- invasive_species_dashboard/ui.R
+++ invasive_species_dashboard/ui.R
@@ -10,49 +10,97 @@
 # Setup and Configuration -------------------------------------------------
 
 source(
-    here::here(
-        "scripts",
-        "0-00_setup_and_configuration.R"
-    ),
+    "0-00_setup_and_configuration.R",
     echo = TRUE,
     max.deparse.length = 1e4
 )
 
-library(shiny)
-
 # Read in dashboard_data
+# To acquire the most-up-to-date data, you must run all the .R files in the
+# scripts folder in the DS4B-final-project GitHub repository.
 dashboard_data <- read_csv(
     "dashboard_data.csv"
 )
 
 
 
-# Ref [1]: Define UI for application that draws a histogram
+# RShiny UI ---------------------------------------------------------------
+
+# Ref [1, 2]: Define UI for application
 fluidPage(
 
     # Application title
     titlePanel("Invasive Species Dashboard"),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar with a select input for species and state
     sidebarLayout(
         sidebarPanel(
+            helpText(
+                "Select a specific invasive",
+                strong("species"),
+                "and",
+                strong("state/territory"),
+                "from the dropdown lists below."
+            ),
+            
+            br(),
+            
             selectInput("simpleName",
-                        label = "Species Name",
+                        label = "Invasive Species",
                         choices = unique(dashboard_data$simpleName)
                         ),
+            
             selectInput("state",
-                        label = "State Name",
-                        choices = unique(dashboard_data$state)
-            )
+                        label = "State/Territory",
+                        choices = list(
+                            "New South Wales",
+                            "Victoria",
+                            "Queensland",
+                            "South Australia",
+                            "Western Australia",
+                            "Tasmania",
+                            "Northern Territory",
+                            "Australian Capital Territory"
+                        ),
+                        selected = "Australian Capital Territory"
+            ),
+            
+            checkboxInput("show_data",
+                          label = "Show temporal data table",
+                          value = TRUE
+                          )
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
+            # Spatial Visualisation
+            h3("Spatial Visualisation"),
+            p(
+                "The below visualisation spatially showcases the number of records for a specific invasive animal",
+                strong("species"),
+                "in a specific",
+                strong("state.")
+                ),
+            br(),
             plotOutput("spatial_visualisation"),
-            plotOutput("temporal_visualisation")
+            
+            # Temporal Visualisation
+            h3("Temporal Visualisation"),
+            p(
+                "The below visualisation temporally showcases the proportion of records for a specific invasive animal in a specific state by month.",
+                "Each coloured bar represents the proportion of records in that specific month with colour showing the relative magnitudes of the proportions."
+                ),
+            p("- A brighter, yellow colour indicates a higher proportion."),
+            p("- A darker, red colour indicates a relatively lower proportion."),
+            br(),
+            plotOutput("temporal_visualisation"),
+            
+            # Data Table
+            h4("Count of invasive species by month"),
+            dataTableOutput("dashboard_data_output")
         )
     )
 )
 
 # References:
 # - [1] https://mastering-shiny.org/basic-app.html
+# - [2] https://shiny.posit.co/r/getstarted/shiny-basics/lesson2/
