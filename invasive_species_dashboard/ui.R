4d861e79a7f8ae49b0c95ffd91d621f3df03c537
Johann Wagner <johann.wagner@gmail.com>
2023-10-18 00:01:11 +1100

    Add abs reference

--- invasive_species_dashboard/ui.R
+++ invasive_species_dashboard/ui.R
@@ -41,7 +41,7 @@ fluidPage(
         p(
           "This RShiny App Dashboard creates two visualisations (spatial and temporal) for seven invasive animal species in Australia.",
           "You can select one of the seven animal species and one of the eight state/territory values to change the visualisation outputs."
-          ),
+        ),
         p("More information on the development of this App can be found on the GitHub Repository: https://github.com/johann-wagner/DS4B-final-project")
       )
     ),
@@ -119,10 +119,9 @@ fluidPage(
       p("4.  Feral Cats (https://doi.org/10.26197/ala.57a58cdc-88c0-45c1-9260-bd17c6fc4b62)"),
       p("5.  Feral Horses (https://doi.org/10.26197/ala.c3cebb21-c2e0-41d0-8b5d-f446c2914827)"),
       p("6.  Feral Pigs (https://doi.org/10.26197/ala.952aa727-1915-4c3f-8399-ed940c24bfef)"),
-      p("7.  Red Imported Fire Ants (https://doi.org/10.26197/ala.74b5dd29-4287-473d-8391-9184c8c62554)")
-        
-        
-    ),
+      p("7.  Red Imported Fire Ants (https://doi.org/10.26197/ala.74b5dd29-4287-473d-8391-9184c8c62554)"),
+      p("ABS Australian Statistical Geography Standard (ASGS) Edition 3 (https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026)")
+      ),
 
     # Main Panel
     mainPanel(
