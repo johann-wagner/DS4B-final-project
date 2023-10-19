25c21545ef1a41b8cf20de7846aaf28070692284
Johann Wagner <johann.wagner@gmail.com>
2023-10-20 10:15:56 +1100

    Use styler

--- scripts/3-01_data_wrangling.R
+++ scripts/3-01_data_wrangling.R
@@ -58,7 +58,7 @@ source(
 # Cleaned Data ------------------------------------------------------------
 
 # Remove the excluded_data from tidied_data
-cleaned_data <- tidied_data %>% 
+cleaned_data <- tidied_data %>%
   anti_join(
     excluded_data,
     join_by(recordID)
@@ -70,7 +70,7 @@ cleaned_data <- tidied_data %>%
 
 # Save Cleaned Data -----------------------------------------------------------
 
-cleaned_data %>% 
+cleaned_data %>%
   write_csv(
     here(
       "processed_data",
