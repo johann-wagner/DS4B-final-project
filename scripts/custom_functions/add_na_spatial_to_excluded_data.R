9be2b6c23ab23358834946762b8e224ca5e1e035
Johann Wagner <johann.wagner@gmail.com>
2023-10-20 10:16:47 +1100

    Use styler

--- scripts/custom_functions/add_na_spatial_to_excluded_data.R
+++ scripts/custom_functions/add_na_spatial_to_excluded_data.R
@@ -13,25 +13,23 @@
 # add_na_spatial_to_excluded_data ----------------------------------------
 
 ### Purpose:
-# To add NA values from the decimalLatitude and decimalLongitude columns into 
+# To add NA values from the decimalLatitude and decimalLongitude columns into
 # the excluded data dataset.
-# Note: This does not remove/filter any data from the input data, this only 
+# Note: This does not remove/filter any data from the input data, this only
 #       adds data to the excluded_data dataset.
 
 add_na_spatial_to_excluded_data <- function(species_occurrence) {
-  
-  add_excluded <- species_occurrence %>% 
+  add_excluded <- species_occurrence %>%
     filter(
       is.na(decimalLatitude),
       is.na(decimalLongitude)
-    ) %>% 
-    
+    ) %>%
     # Add excluded_notes
     mutate(
       date           = Sys.Date(),
       excluded_notes = "These data points have missing spatial values."
     )
-  
+
   excluded_data <- bind_rows(excluded_data, add_excluded)
   return(excluded_data)
 }
