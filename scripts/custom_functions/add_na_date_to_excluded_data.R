7497ccedc506805801509528a60f77c2740167d5
Johann Wagner <johann.wagner@gmail.com>
2023-10-20 10:16:19 +1100

    Use styler

--- scripts/custom_functions/add_na_date_to_excluded_data.R
+++ scripts/custom_functions/add_na_date_to_excluded_data.R
@@ -14,20 +14,18 @@
 
 ### Purpose:
 # To add NA values from the eventDate column into the excluded data dataset.
-# Note: This does not remove/filter any data from the input data, this only 
+# Note: This does not remove/filter any data from the input data, this only
 #       adds data to the excluded_data dataset.
 
 add_na_date_to_excluded_data <- function(species_occurrence) {
-  
-  add_excluded <- species_occurrence %>% 
-    filter(is.na(eventDate)) %>% 
-    
+  add_excluded <- species_occurrence %>%
+    filter(is.na(eventDate)) %>%
     # Add excluded_notes
     mutate(
       date           = Sys.Date(),
       excluded_notes = "These data points have a missing eventDate value."
     )
-  
+
   excluded_data <- bind_rows(excluded_data, add_excluded)
   return(excluded_data)
 }
